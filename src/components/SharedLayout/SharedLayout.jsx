25921190d76aa942b2eec72355983bb8880370cd
Andrey9019 <1234andrey5678@gmail.com>
2024-10-24 19:48:28 +0300

    add: add  BackgroundCircles

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,18 +4,20 @@ import Loader from '../Loader/Loader';
 
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
+import BackgroundCircles from 'components/BackgroundCircles/BackgroundCircles';
 
 const SharedLayout = () => {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen relative">
+        <BackgroundCircles />
+
         <Header />
         <main className="flex-grow">
           <Suspense fallback={<Loader />}>
             <Outlet />
           </Suspense>
         </main>
-
         <Footer />
       </div>
     </>
