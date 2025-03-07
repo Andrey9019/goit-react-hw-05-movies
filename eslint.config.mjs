84317e0f22b0ef05d6c9d59f399b7289caf93f68
Andrey9019 <1234andrey5678@gmail.com>
2025-03-07 14:07:07 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// export default {
+//   extends: ["next/core-web-vitals", "next/typescript", ...compat.extends()],
+//   rules: {
+//     "@typescript-eslint/no-unused-vars": "off",
+//     "react/no-unescaped-entities": "off",
+//   },
+// };
 
-export default {
-  ...eslintConfig,
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off",
-    "react/no-unescaped-entities": "off",
-  },
-};
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const compat = new FlatCompat();
+
+// export default [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     rules: {
+//       "@typescript-eslint/no-unused-vars": "off",
+//       "react/no-unescaped-entities": "off",
+//     },
+//   },
+// ];
+
+// const eslintConfig = [
+//   "next/core-web-vitals",
+//   "next/typescript",
+//   ...compat.extends(),
+// ];
+
+// export default {
+//   ...eslintConfig,
+//   rules: {
+//     "@typescript-eslint/no-unused-vars": "off",
+//     "react/no-unescaped-entities": "off",
+//   },
+// };
